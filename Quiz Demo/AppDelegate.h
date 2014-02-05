2fec74ec132079e08c3ea0ba6cb18ae5fdac425f
Jorge Casariego <jorgekasa@gmail.com>
2014-02-05 12:34:49 +0100

    Initial Commit

---
+++ Quiz Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quiz Demo
+//
+//  Created by Jorge Casariego on 05/02/14.
+//  Copyright (c) 2014 Jorge Casariego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quiz Demo
+//
+//  Created by Jorge Casariego on 05/02/14.
+//  Copyright (c) 2014 Jorge Casariego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
